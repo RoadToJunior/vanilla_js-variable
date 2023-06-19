91027ec2b4dcc11e16b3f3f10d9c5bee32516789
NowWeAre-Free <kontodarm@wp.pl>
2023-06-19 16:12:31 +0200

    excersise 1

---
+++ app.js
@@ -0,0 +1,11 @@
+// Zadanie 1: Deklaracja zmiennych
+
+// 1.1. Zadeklaruj zmienną o nazwie myVariable i przypisz jej wartość 10 (typ number).
+
+let myVariable = 10;
+
+// 1.2. Zadeklaruj zmienną o nazwie myName i przypisz jej wartość "Benedykt".
+
+let myName = "Benedykt";
+
+// Uwaga: Obie zmienne mogą zmieniać wartości w programie, a więc nie mogą to być stałe.
